cddb1119a6ec38106ac54099811d3620c3e439b2
Greyson Elkins <greysonelkins@gmail.com>
2020-12-05 16:10:33 -0700

    Add desk and laptop

--- src/components/Office/Office.tsx
+++ src/components/Office/Office.tsx
@@ -1,13 +1,38 @@
 import React from 'react'
 
 import desk from '../../images/office/desk.png'
+import speakerL from '../../images/office/speaker-L.png'
+import speakerR from '../../images/office/speaker-R.png'
+import laptop from '../../images/office/laptop.png'
+
 import './Office.scss'
 
 const Office: React.FC = () => {
   return (
     <main>
       <div className="room">
-        <img src={desk} alt="A mid-century modern desk"/>
+        <div className="desk-top">
+          {/* <img 
+            src={speakerL} 
+            alt="A desk-top speaker" 
+            className="speaker"
+            /> */}
+          <img 
+            src={laptop} 
+            alt="laptop" 
+            className="laptop"
+          />
+          {/* <img 
+            src={speakerR} 
+            alt="A desk-top speaker" 
+            className="speaker"
+          /> */}
+        </div>
+        <img 
+          src={desk} 
+          alt="A mid-century modern desk" 
+          className="desk"
+        />
       </div>
     </main>
   )
